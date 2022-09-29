fc5f1767de9db97628f20995606b46679bda8371
dci-student <hollidaylucy.com>
2022-09-29 11:38:24 +0200

    up to date version

--- src/App.js
+++ src/App.js
@@ -2,36 +2,27 @@ import { useState } from "react";
 //import ToDoForm from "./components/ToDoForm";
 //import ToDoList from "./components/ToDoList";
 import Editor from "./components/Editor";
-// import "./App.css";
-
-
+import "./App.css";
 
 function App() {
-
   const [toDoList, setToDoList] = useState([]);
 
   const addToDo = (todo) => {
-    setToDoList(toDoList => [...toDoList, todo]);
-
-  }
+    setToDoList((toDoList) => [...toDoList, todo]);
+  };
 
   const completedToDo = (index) => {
     const newToDo = toDoList.filter((todo, i) => i !== index);
     setToDoList(newToDo);
-  }
-
+  };
 
   return (
-    <div className="App">
- 
-
-
-      <Editor todoList={toDoList} addToDo={addToDo} completedToDo={completedToDo}/>
-
-      </div>
-
-
-  )
+    <Editor
+      todoList={toDoList}
+      addToDo={addToDo}
+      completedToDo={completedToDo}
+    />
+  );
 }
 
 export default App;
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,54 +1,55 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-function Editor({toDoList, addToDo, completedToDo}){
-    const [input, addToList] = useState('');
+function Editor({ toDoList, addToDo, completedToDo }) {
+  const [input, addToList] = useState("");
 
+  const handleChange = (e) => {
+    if (e.target.name === "input") {
+      addToList(e.target.value);
+    } else {
+      alert("Please write a to-do");
+    }
+  };
 
-const handleChange = (e) => {
-    if (e.target.name === 'input') {
- addToList(e.target.value);
-} else {alert('Please write a to-do');}
+  const createToDo = () => {
+    addToDo({ input: input });
+    addToList("");
+  };
 
-}
-
-const createToDo = () => {
-    addToDo({input: input});
-    addToList('');
-}
-
-const remove = toDoList.map((todo, i) => (
+  const remove = toDoList.map((todo, i) => (
     <tr key={i}>
-         <td>{todo.input}</td>
-         <td><button onClick={() => completedToDo(i)}>Completed</button></td>
+      <td>{todo.input}</td>
+      <td>
+        <button onClick={() => completedToDo(i)}>Completed</button>
+      </td>
     </tr>
-))
+  ));
 
-return (
+  return (
     <div className="container">
-        <h2>Date here</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>To-Do</th>
-                    <th>Completed</th>
-                </tr>
-            </thead>
-            <tbody>
-                {remove}
-            </tbody>
-        </table>
-        <div>
-            <input onChange={handleChange} name="front" value={input} placeholder="Front of Card" />
+      <h2>Date here</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>To-Do</th>
+            <th>Completed</th>
+          </tr>
+        </thead>
+        <tbody>{remove}</tbody>
+      </table>
+      <div>
+        <input
+          onChange={handleChange}
+          name="front"
+          value={input}
+          placeholder="Front of Card"
+        />
 
-            <button onClick={createToDo} >Add Card</button>
-        </div>
-        <hr />
-
+        <button onClick={createToDo}>Add Card</button>
+      </div>
+      <hr />
     </div>
-)
+  );
 }
-
-
-
 
 export default Editor;
